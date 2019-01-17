83c2cbf3ae886adccb38245f7b2b0f37ae30bf6e
johnsokk <johnsokk@rose-hulman.edu>
2019-01-17 09:36:56 -0500

    Welcome to CSSE 120

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -22,7 +22,7 @@
 ###############################################################################
 
 
-print('Hello, World')
+print('Hello, Jonny')
 print('hi there')
 print('one', 'two', 'buckle my shoe')
 

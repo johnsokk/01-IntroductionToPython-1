28c784500a622c0679a8911634e849b970a1171f
johnsokk <johnsokk@rose-hulman.edu>
2019-01-18 09:10:29 -0500

    Welcome to CSSE 120

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Kaia Johnson.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,4 +43,21 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+conan = rg.SimpleTurtle()
+conan.pen = rg.Pen('blue', 10)
+conan.left(90)
+conan.forward(200)
+conan.pen_up()
+conan.go_to(rg.Point(100, -40))
+conan.pen_down()
+conan.pen = rg.Pen('green', 10)
+conan.right(180)
+conan.forward(150)
+window.close_on_mouse_click()
+
+
 
